4d70a3e75ab934ecdb52be3605be26e0ebbeee84
Shahin <shahin.ganiyev1@gmail.com>
2025-05-27 13:08:11 +0200

    Task

---
+++ Repository/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Repository;
+
+Console.WriteLine("Hello, World!");
+
+
+
+Student student = new()
+{
+
+}
+Show();
+
---
+++ Repository/Student.cs
@@ -0,0 +1,30 @@
+﻿
+namespace Repository
+{
+    public class Student
+    {
+        public string Name;
+
+        public string Surname;
+
+        public byte age;
+    }
+
+    public Student(string surname, string name, byte age)
+
+        {
+            Name = name;
+            Surname = surname;
+            age = age;
+
+            Console.WriteLine($"Name:{name}, Surname:{surname}, Age:{age}");
+
+        }
+
+
+        void Show()
+        {
+            Student student = new Student("John", 20);
+        }
+
+    }
